176db09d03e1eab8fe6984aaff821444d9d2afc6
Ziad Gaafar <ziadgaafar@protonmail.com>
2025-03-06 15:30:09 +0200

    fix(App): set isCartOpen to true when updating cart items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -70,6 +70,7 @@ class App extends Component<Record<string, never>, AppState> {
       return {
         ...prevState,
         cartItems: newCartItems,
+        isCartOpen: true,
       };
     });
   };
