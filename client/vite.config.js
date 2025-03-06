74694683fb401728cb4fc01c289e55f194e46f61
Ziad Gaafar <ziadgaafar@protonmail.com>
2025-03-06 13:56:24 +0200

    add Vite configuration and remove unused React Router type definitions

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "url";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "http://localhost:80",
+                changeOrigin: true,
+            },
+        },
+    },
+});
