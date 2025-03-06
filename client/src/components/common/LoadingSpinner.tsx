164019a42b5f489837cdfe2a7db1ed3e8f4ce012
Ziad Gaafar <ziadgaafar@protonmail.com>
2025-03-06 11:57:58 +0200

    refactor LoadingSpinner component to remove unnecessary min-height styling

--- client/src/components/common/LoadingSpinner.tsx
+++ client/src/components/common/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 class LoadingSpinner extends Component {
   render() {
     return (
-      <div className="flex justify-center items-center min-h-screen w-full">
+      <div className="flex justify-center items-center w-full">
         <div className="size-12 border-3 border-border border-t-primary rounded-full animate-spin"></div>
       </div>
     );
